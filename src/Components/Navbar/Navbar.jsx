ee6a0ab15fcd959ba13a160670ed4fc0155d2fc6
vagabond0091 <endayajerry11@gmail.com>
2024-01-03 14:31:18 +0800

    Adding a navbar and state on each element

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 const Navbar = () => {
+  const [menu,setMenu]  = useState("shop");
   return (
     <div className="navbar"> 
         <div className="nav-logo">
@@ -10,10 +11,10 @@ const Navbar = () => {
             <p>Shopper</p>
         </div>
         <ul className="nav-menu">
-            <li>Shop<hr/></li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
+            <li onClick={() => {setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+            <li onClick={() => {setMenu("mens")}}>Men{menu==="mens"?<hr/>:<></>}</li>
+            <li onClick={() => {setMenu("womens")}}>Women{menu==="womens"?<hr/>:<></>}</li>
+            <li onClick={() => {setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
             <button>Login</button>
