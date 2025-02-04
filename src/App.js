0e27c1e5d4288ab9228cbae05d38b8ea1fdd7d50
Jerry Endaya <vagabondendaya@gmail.com>
2025-02-04 12:51:45 +0800

    Github pages

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
-import Product from './Pages/Product';
-import Cart from './Pages/Cart';
+
 import Login from './Pages/Login';
 import Footer from './Components/Footer/Footer';
 import men_banner from './Components/Assets/banner_mens.png'
@@ -20,10 +19,10 @@ function App() {
           <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
           <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
           <Route path='/kids' element={<ShopCategory  banner={kid_banner}category="kid"/>}/>
-          <Route path="product" element={<Product/>}>
+          {/* <Route path="product" element={<Product/>}>
               <Route path=':productId' element={<Product/>}/>
-          </Route>
-          <Route path="cart" element={<Cart/>}/>
+          </Route> */}
+          {/* <Route path="cart" element={<Cart/>}/> */}
           <Route path="login" element={<Login/>}/>
         </Routes>
         <Footer/>
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
-import { ShopContext } from '../Context/ShopContext'
+// import { ShopContext } from '../Context/ShopContext'
 import dropdown_icon from '../Components/Assets/dropdown_icon.png'
 import './css/ShopCategory.css'
 import Item from '../Components/Item/Item';
 const ShopCategory = (props) => {
-  const {all_product} = useContext(ShopContext);
+  // const {all_product} = null;
   return (
     <div className='shop-category'>
         <img className='shopcategory-banner' src={props.banner} alt="" />
@@ -17,14 +17,14 @@ const ShopCategory = (props) => {
           </div>
         </div>
         <div className="shopcategory-products">
-          {all_product.map((item,i)=>{
+          {/* {all_product.map((item,i)=>{
             if(props.category === item.category){
               return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
             }
             else{
               return null;
             }
-          })}
+          })} */}
         </div>
         <div className="shopcategory-loadMore">
           Explore More
